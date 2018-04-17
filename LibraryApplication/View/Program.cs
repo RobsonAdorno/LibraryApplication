ba6030a545b12461f35297517de7b7351530054c
Robson Adorno Ferrera <robson-macbook@MacBook-Pro-de-Robson.local>
2018-04-17 01:03:21 -0300

    Primeiro commit do projeto novo

---
+++ LibraryApplication/View/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LibraryApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            do
+            {
+                Console.WriteLine("Selecione abaixo a opção desejada!");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("1 – Cadastrar Livro");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("2 – Emprestar Livro");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("3 – Devolver Livro");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("4 – Histórico de movimentação do livro");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("5 – Consultar Livro");
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("0 – Sair");
+                 op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+
+                        break;
+                    case "0":
+                        Console.WriteLine("Encerrando...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção não encontrada!");
+                        break;
+                }
+
+            } while (op != "0");
+
+
+
+
+
+
+        }
+    }
+}
